93f32afed026642080fca2eb8adca79f266eab5e
Fer <ibanez.fernando.2018@gmail.com>
2024-06-11 12:53:33 -0300

    Empezando a escribir

---
+++ Fabrica.cs
@@ -0,0 +1,5 @@
+using espacioPersonajes;
+namespace espacioFabrica
+{
+     
+}
---
+++ Personajes.cs
@@ -0,0 +1,21 @@
+namespace espacioPersonajes
+{
+    public class Personaje
+    {
+        private Datos datosPersonaje = new Datos();
+        private Caracteristicas caracteristicasPersonaje = new Caracteristicas();
+
+        public Datos DatosPersonaje { get => datosPersonaje; set => datosPersonaje = value; }
+        public Caracteristicas CaracteristicasPersonaje { get => caracteristicasPersonaje; set => caracteristicasPersonaje = value; }
+    }
+    public class Datos
+    {
+        string? tipo, nombre, apodo;
+        DateTime fechaNac;
+        int edad;//Max 300
+    }
+    public class Caracteristicas
+    {
+        int velocidad, destreza, fuerza, nivel, armadura, salud; // 1-10, 1-5, 1-10,1-10, 1-10, 100 respectivamente
+    }
+}
