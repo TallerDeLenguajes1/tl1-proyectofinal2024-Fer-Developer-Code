790a3e8fac72e3169237fea9c8ec62d418bd84eb
Fernando <ibanez.fernando.2018@gmail.com>
2024-06-20 17:20:09 -0300

    Añadida con exito api para consultar nombre

--- Program.cs
+++ Program.cs
@@ -1,5 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EspacioArteAscii;
+using EspacioApiNombres;
+using System.Text.Json;
 ArteAscii ImgJuego = new ArteAscii();
 ImgJuego.MostrarLogo();
-string linea = Console.ReadLine();
+
+NombrePersonaje NombreObtenido = await GetNombreAsync();
+
+
+static async Task<NombrePersonaje> GetNombreAsync()
+{
+    string url = "https://api.namefake.com/";
+    try
+    {
+        HttpClient client = new HttpClient();
+
+        HttpResponseMessage response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+
+        string responseBody = await response.Content.ReadAsStringAsync();
+
+        NombrePersonaje nombrePersonaje = JsonSerializer.Deserialize<NombrePersonaje>(responseBody);
+        return nombrePersonaje;
+    }
+    catch (HttpRequestException e)
+    {
+        Console.WriteLine("Problemas de acceso a la API");
+        Console.WriteLine("Message :{0} ", e.Message);
+        return null;
+    }
+}
