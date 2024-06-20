56743c9f7831299481a27dc5972a668ad0f3919b
Fernando <ibanez.fernando.2018@gmail.com>
2024-06-20 18:49:24 -0300

    avanzando hasta llenar clases

--- EspacioFabricaPersonajes.cs
+++ EspacioFabricaPersonajes.cs
@@ -1,10 +1,34 @@
 using EspacioPersonajes;
 namespace espacioFabricaPersonajes
 {
-    public class FabricaDePersonajes()
+    public class FabricaDePersonajes
     {
-        Random random = new Random();
-        List<Personaje> ListaPersonajes = new List<Personaje>();
-        
+        private List<Personaje> ListaPersonajes = new List<Personaje>();
+        private Random random = new Random();
+
+        public FabricaDePersonajes()
+        {
+            RazasPersonaje razaAleatoria = (RazasPersonaje)random.Next(Enum.GetNames(typeof(RazasPersonaje)).Length);
+            switch (razaAleatoria)//El switch y otras estructuras solo funcionan una vez dentro del constructor o cualquier estructura que no sea una clase
+            {
+                case RazasPersonaje.Humano:
+
+                    break;
+                case RazasPersonaje.Elfo:
+
+                    break;
+                case RazasPersonaje.Enano:
+
+                    break;
+                case RazasPersonaje.Orco:
+
+                    break;
+                case RazasPersonaje.Troll:
+
+                    break;
+                case RazasPersonaje.Goblin:
+                break;
+            }
+        }
     }
 }
--- EspacioPersonajes.cs
+++ EspacioPersonajes.cs
@@ -2,19 +2,46 @@ namespace EspacioPersonajes
 {
     public class Personaje
     {
-        private Datos datosPersonaje = new Datos();
-        private Caracteristicas caracteristicasPersonaje = new Caracteristicas();
+        private Datos datosPersonaje;
+        private Caracteristicas caracteristicasPersonaje;
+
+        public Personaje(RazasPersonaje raza, string nombre, string apodo, DateTime fechaNac,
+                        int edad, int velocidad, int destreza, int fuerza, int nivel, int armadura, int salud)
+        {
+            datosPersonaje = new Datos(raza, nombre, apodo, fechaNac, edad);
+            caracteristicasPersonaje = new Caracteristicas(velocidad, destreza, fuerza, nivel, armadura, salud);
+        }
+
         public void Atacar()
         {
+            Random random = new Random();
+            int ataque = caracteristicasPersonaje.Destreza * caracteristicasPersonaje.Fuerza * caracteristicasPersonaje.Nivel;
+            int efectividad = random.Next(1, 101);
 
         }
     }
+
     public class Datos
     {
         private RazasPersonaje raza;
-        private string? nombre, apodo;
+        private string nombre, apodo;
         private DateTime fechaNac;
         private int edad;//Max 300
+
+        public Datos(RazasPersonaje raza, string nombre, string apodo, DateTime fechaNac, int edad)
+        {
+            this.raza = raza;
+            this.nombre = nombre;
+            this.apodo = apodo;
+            this.fechaNac = fechaNac;
+            this.edad = edad;
+        }
+
+        public RazasPersonaje Raza { get => raza; }
+        public string Nombre { get => nombre; }
+        public string Apodo { get => apodo; }
+        public DateTime FechaNac { get => fechaNac; }
+        public int Edad { get => edad; }
     }
     public class Caracteristicas
     {
@@ -24,24 +51,31 @@ namespace EspacioPersonajes
         private int nivel;       // Rango: 1-10
         private int armadura;    // Rango: 1-10
         private int salud;       // Valor máximo: 100
+
+        public Caracteristicas(int velocidad, int destreza, int fuerza, int nivel, int armadura, int salud)
+        {
+            this.velocidad = velocidad;
+            this.destreza = destreza;
+            this.fuerza = fuerza;
+            this.nivel = nivel;
+            this.armadura = armadura;
+            this.salud = salud;
+        }
+
+        public int Velocidad { get => velocidad; }
+        public int Destreza { get => destreza; }
+        public int Fuerza { get => fuerza; }
+        public int Nivel { get => nivel; }
+        public int Armadura { get => armadura; }
+        public int Salud { get => salud; }
     }
     public class PersonajesJson
     {
 
     }
+
     public enum RazasPersonaje
     {
-        Humano,
-        Elfo,
-        Enano,
-        Orco,
-        Goblin,
-        Centauro,
-        Minotauro,
-        Vampiro,
-        Licántropo,
-        Troll,
-        Gólem,
-        Harpía
+        Humano, Elfo, Enano, Orco, Goblin, Centauro, Minotauro, Vampiro, Licántropo, Troll, Gólem, Harpía
     }
 }
