f84f00ba3dcdfee1a954e9a565b3b4b3a3b84d2a
Victor Ibañez <ibanez.fernando.2018@gmail.com>
2024-06-20 22:16:20 -0300

    Acomodando

---
+++ DatosYCaracteristicas.cs
@@ -0,0 +1,52 @@
+using EspacioPersonajes;
+namespace DatosYCaracteristicas
+{
+    public class Datos
+    {
+        private RazasPersonaje raza;
+        private string nombre, apodo;
+        private DateTime fechaNac;
+        private int edad;//Max 300
+
+        public Datos(RazasPersonaje raza, string nombre, string apodo, DateTime fechaNac, int edad)
+        {
+            this.raza = raza;
+            this.nombre = nombre;
+            this.apodo = apodo;
+            this.fechaNac = fechaNac;
+            this.edad = edad;
+        }
+
+        public RazasPersonaje Raza { get => raza; }
+        public string Nombre { get => nombre; }
+        public string Apodo { get => apodo; }
+        public DateTime FechaNac { get => fechaNac; }
+        public int Edad { get => edad; }
+    }
+    public class Caracteristicas
+    {
+        private int velocidad;   // Rango: 1-10
+        private int destreza;    // Rango: 1-5
+        private int fuerza;      // Rango: 1-10
+        private int nivel;       // Rango: 1-10
+        private int armadura;    // Rango: 1-10
+        private int salud;       // Valor máximo: 100
+
+        public Caracteristicas(int velocidad, int destreza, int fuerza, int nivel, int armadura, int salud)
+        {
+            this.velocidad = velocidad;
+            this.destreza = destreza;
+            this.fuerza = fuerza;
+            this.nivel = nivel;
+            this.armadura = armadura;
+            this.salud = salud;
+        }
+
+        public int Velocidad { get => velocidad; }
+        public int Destreza { get => destreza; }
+        public int Fuerza { get => fuerza; }
+        public int Nivel { get => nivel; }
+        public int Armadura { get => armadura; }
+        public int Salud { get => salud; }
+    }
+}
--- EspacioPersonajes.cs
+++ EspacioPersonajes.cs
@@ -1,18 +1,19 @@
+using DatosYCaracteristicas;
 namespace EspacioPersonajes
 {
     public class Personaje
     {
-        private Datos datosPersonaje;
-        private Caracteristicas caracteristicasPersonaje;
+        private Datos datos;
+        private Caracteristicas caracteristicas;
 
         public Personaje(Datos datosPersonaje, Caracteristicas caracteristicasPersonaje)
         {
-            this.datosPersonaje = datosPersonaje;
-            this.caracteristicasPersonaje = caracteristicasPersonaje;
+            this.datos = datosPersonaje;
+            this.caracteristicas = caracteristicasPersonaje;
         }
 
-        public Datos DatosPersonaje { get => datosPersonaje; }
-        public Caracteristicas CaracteristicasPersonaje { get => caracteristicasPersonaje; }
+        public Datos DatosPersonaje { get => datos; }
+        public Caracteristicas CaracteristicasPersonaje { get => caracteristicas; }
 
         public void Atacar()
         {
@@ -23,54 +24,6 @@ namespace EspacioPersonajes
         }
     }
 
-    public class Datos
-    {
-        private RazasPersonaje raza;
-        private string nombre, apodo;
-        private DateTime fechaNac;
-        private int edad;//Max 300
-
-        public Datos(RazasPersonaje raza, string nombre, string apodo, DateTime fechaNac, int edad)
-        {
-            this.raza = raza;
-            this.nombre = nombre;
-            this.apodo = apodo;
-            this.fechaNac = fechaNac;
-            this.edad = edad;
-        }
-
-        public RazasPersonaje Raza { get => raza; }
-        public string Nombre { get => nombre; }
-        public string Apodo { get => apodo; }
-        public DateTime FechaNac { get => fechaNac; }
-        public int Edad { get => edad; }
-    }
-    public class Caracteristicas
-    {
-        private int velocidad;   // Rango: 1-10
-        private int destreza;    // Rango: 1-5
-        private int fuerza;      // Rango: 1-10
-        private int nivel;       // Rango: 1-10
-        private int armadura;    // Rango: 1-10
-        private int salud;       // Valor máximo: 100
-
-        public Caracteristicas(int velocidad, int destreza, int fuerza, int nivel, int armadura, int salud)
-        {
-            this.velocidad = velocidad;
-            this.destreza = destreza;
-            this.fuerza = fuerza;
-            this.nivel = nivel;
-            this.armadura = armadura;
-            this.salud = salud;
-        }
-
-        public int Velocidad { get => velocidad; }
-        public int Destreza { get => destreza; }
-        public int Fuerza { get => fuerza; }
-        public int Nivel { get => nivel; }
-        public int Armadura { get => armadura; }
-        public int Salud { get => salud; }
-    }
     public class PersonajesJson
     {
 
