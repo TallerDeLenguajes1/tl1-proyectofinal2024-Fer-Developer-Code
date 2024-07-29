22e70ce0b12b191e23183c0b5d25868166094452
Victor Ibañez <ibanez.fernando.2018@gmail.com>
2024-07-29 20:11:22 -0300

    Acomodado

--- Game/Inicio.cs
+++ Game/Inicio.cs
@@ -3,6 +3,7 @@ using EspacioJsonCreacion;
 using EspacioTorneo;
 using EspacioArteAscii.GUI;
 using static System.Console;
+using EspacioMenu;
 
 namespace EspacioInicio
 {
@@ -10,12 +11,29 @@ namespace EspacioInicio
     {
         public async Task InicializarJuego(PersonajesJson archivos, Torneo torneo, string rutaListaPjs, string rutaJugador, string rutaGanadores)
         {
+            Console.Clear();
             Title = "CodeRealm: El Reino de las Clases";
             //Title = "Jueguito"; Añade el titulo a la terminal de una clase, ejecutar despues
             ArteAscii ascii = new ArteAscii();
             ascii.MostrarLogo();
             Juego menuPrincipal = new Juego();
             await menuPrincipal.RunMainMenu(archivos, torneo, rutaListaPjs, rutaJugador, rutaGanadores);
+
+            //Preguntar si desea seguir jugando o salir
+            string[] opciones = { "Seguir Jugando", "Salir" };
+            string elegir = "¿Desea seguir jugando?";
+            string[] portada = ascii.asciiMago;
+
+            MenuGrafico menuJuego = new MenuGrafico(portada, elegir, opciones);
+            int opcion = menuJuego.Run();
+            switch (opcion)
+            {
+                case 0:
+                    await menuPrincipal.RunMainMenu(archivos, torneo, rutaListaPjs, rutaJugador, rutaGanadores);
+                    break;
+                case 1:
+                    break;
+            }
         }
     }
 }
